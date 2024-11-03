ac80b0fbed215e412daa45605a39090890538022
Sherif Samy <115134113+SHERIF-SAMY@users.noreply.github.com>
2024-11-03 23:28:07 +0200

    task_2 VB  ---> [ Image Slider ]

---
+++ imageSlideShow/imageSlideShow/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace imageSlideShow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Previous = new System.Windows.Forms.Button();
+            this.Next = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(741, 285);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Previous
+            // 
+            this.Previous.Location = new System.Drawing.Point(145, 369);
+            this.Previous.Name = "Previous";
+            this.Previous.Size = new System.Drawing.Size(95, 44);
+            this.Previous.TabIndex = 1;
+            this.Previous.Text = "Previous";
+            this.Previous.UseVisualStyleBackColor = true;
+            this.Previous.Click += new System.EventHandler(this.Previous_Click);
+            // 
+            // Next
+            // 
+            this.Next.Location = new System.Drawing.Point(511, 369);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(119, 44);
+            this.Next.TabIndex = 2;
+            this.Next.Text = "Next";
+            this.Next.UseVisualStyleBackColor = true;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Next);
+            this.Controls.Add(this.Previous);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Previous;
+        private System.Windows.Forms.Button Next;
+    }
+}
+
---
+++ imageSlideShow/imageSlideShow/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace imageSlideShow
+{
+    public partial class Form1 : Form
+    {
+        
+        List<string> imagePaths = new List<string> {
+            "C:\\Users\\Asus\\Desktop\\cluint of sticky\\2.png",
+            "C:\\Users\\Asus\\Desktop\\cluint of sticky\\WhatsApp Image 2024-10-22 at 00.30.57_402a17d1.jpg",
+            "C:\\Users\\Asus\\Desktop\\cluint of sticky\\WhatsApp Image 2024-10-22 at 19.02.22_45ae6a24.jpg",
+        };
+
+        int currentImageIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadImage();
+        }
+
+        private void LoadImage()
+        {
+            if (imagePaths.Count > 0)
+            {
+                try
+                {
+                    pictureBox1.Image = Image.FromFile(imagePaths[currentImageIndex]);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading image: " + ex.Message);
+                }
+            }
+        }
+
+        private void Previous_Click(object sender, EventArgs e)
+        {
+            if (currentImageIndex > 0)
+            {
+                currentImageIndex--;
+                LoadImage();
+            }
+            else
+            {
+                MessageBox.Show("No previous image.");
+            }
+        }
+
+        private void Next_Click(object sender, EventArgs e)
+        {
+            if (currentImageIndex < imagePaths.Count - 1)
+            {
+                currentImageIndex++;
+                LoadImage();
+            }
+            else
+            {
+                MessageBox.Show("No next image.");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            LoadImage();
+        }
+    }
+}
